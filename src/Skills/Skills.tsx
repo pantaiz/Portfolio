adf328d8bd2466ab664cc77593182cfbfc4a027c
Pantaiz <pantaiz@yandex.ru>
2023-05-15 18:32:54 +0300

    add image animation

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -3,18 +3,18 @@ import styles from './Skills.module.css';
 import styleContainer from './../common/styles/container.module.css'
 import {Skill} from "./Skill/Skill";
 import {v1} from "uuid";
-import reactIcon from "../common/assets/iconskill/react.png"
-import reduxIcon from "../common/assets/iconskill/redux.png"
-import tsIcon from "../common/assets/iconskill/typescript.png"
-import jsIcon from "../common/assets/iconskill/js.png"
-import storybookIcon from "../common/assets/iconskill/storybook.png"
-import gitIcon from "../common/assets/iconskill/git.png"
-import MUIIcon from "../common/assets/iconskill/MUI.png"
-import HTML from "../common/assets/iconskill/html.png"
-import CSS from "../common/assets/iconskill/css.png"
-import Rest from "../common/assets/iconskill/rest-api.png"
-import unit from "../common/assets/iconskill/unit.png"
-import ant from "../common/assets/iconskill/ant-design.png"
+import reactIcon from "../common/assets/img/iconskill/react.png"
+import reduxIcon from "../common/assets/img/iconskill/redux.png"
+import tsIcon from "../common/assets/img/iconskill/typescript.png"
+import jsIcon from "../common/assets/img/iconskill/js.png"
+import storybookIcon from "../common/assets/img/iconskill/storybook.png"
+import gitIcon from "../common/assets/img/iconskill/git.png"
+import MUIIcon from "../common/assets/img/iconskill/MUI.png"
+import HTML from "../common/assets/img/iconskill/html.png"
+import CSS from "../common/assets/img/iconskill/css.png"
+import Rest from "../common/assets/img/iconskill/rest-api.png"
+import unit from "../common/assets/img/iconskill/unit.png"
+import ant from "../common/assets/img/iconskill/ant-design.png"
 
 
 type TempInitialSkilStateType = Array<{ title: string, icon: string }>
